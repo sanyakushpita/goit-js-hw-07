12872c8d0a45d47fb95f3efb19b24a1b1454358d
SaNya <kushpita.o@gmail.com>
2025-03-12 01:19:41 +0200

    end homework

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', buttonChangeColorClick);
+
+function buttonChangeColorClick() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+
+  buttonChangeColor.blur();
+}
